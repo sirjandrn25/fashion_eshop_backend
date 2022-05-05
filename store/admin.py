9d54bab08e3569a083153c25ae038837f3a03868
sirjan tamang <sirjantmg99@gmail.com>
2022-05-05 13:58:30 +0545

    change qty loading page in cart

--- store/admin.py
+++ store/admin.py
@@ -9,4 +9,13 @@ admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Fashion)
 admin.site.register(Brand)
-admin.site.register(ProductSize)
+# admin.site.register(ProductSize)
+
+
+@admin.register(ProductSize)
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = ['id','size','product_name']
+
+    @admin.display(description="product_name")
+    def product_name(self,obj):
+        return obj.product.title
